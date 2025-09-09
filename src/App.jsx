8871916ce5381dda26b0c1b973f23fc7700409b4
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-09-09 08:36:20 +0300

    initial creation

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Maiden Tiedot</h1>
+      <p>Welcome to the Maiden Tiedot application!</p>
+    </div>
+  )
+}
+
+export default App
