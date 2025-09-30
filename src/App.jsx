58ee4aea8137af6c9f186eeccbb982d4d84b0334
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-09-30 19:28:23 +0300

    weather app is added - tehtävä on valmis

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
@@ -6,6 +6,35 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [error, setError] = useState(null)
   const [selectedCountry, setSelectedCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
+
+  //DEFINE API KEY  
+  const API_KEY = '6b74ee54d236dccd7eb5aaf18a008477'
+
+  //WHEN THE COUNTRY IS SELECTED => FETCH THE WEATHER
+  useEffect (()=>{
+    if(selectedCountry && selectedCountry.capital) {
+      const capital = selectedCountry.capital[0];
+      axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${API_KEY}&units=metric`
+
+      )
+
+      .then((response) =>{
+        setWeather(response.data);
+      })
+
+      .catch((error)=> {
+        console.error(error);
+        setWeather(null);
+      });
+    }
+    }, [selectedCountry]);
+
+
+
+
 
 
   // Add Handlers beside country's name
@@ -30,6 +59,12 @@ const App = () => {
     )
     setCountries(filtered)
     setError(null)
+
+    if (filtered.length === 1) {
+  setSelectedCountry(filtered[0])
+} else {
+  setSelectedCountry(null)
+}
   })
       .catch(() => {
         setCountries([])
@@ -38,7 +73,7 @@ const App = () => {
   }
 
   return (
-    <div className="app">
+    <div>
       <h1>Country's Information</h1>
       <p>Welcome to the Country's Information application!</p>
 
@@ -51,6 +86,8 @@ const App = () => {
       </form>
 
       {error ? <p>{error}</p> : null}
+
+    {/* when a country is selected */}
 
       {selectedCountry ? (
         <div>
@@ -66,12 +103,30 @@ const App = () => {
               width="200"
             />
           )}
-          {/* Back to results button */}
+  {/* weather info */}
+  {weather && (
+    <div>
+
+      <h3>Weather in {selectedCountry.capital[0]}</h3>
+      <p>Temprature: {weather.main.temp} Celisius</p>
+      <p>Feels like: {weather.main.feels_like} Celisius</p>
+      <p>Condition: {weather.weather[0].description}</p>
+     
+      <img src="//openweathermap.org/img/wn/02n@2x.png" width="200" height="200"/>
+      <p>Wind: {weather.wind.speed} m/s</p>
+
+    </div>
+  )}
+
+
+          {/* Back button */}
           <div style={{ marginTop: '10px' }}>
               <button type="button" onClick={handleClose}>Back to results</button>
           </div>
         </div>
       ) : null}
+
+{/* search results */}
 
       {countries.length > 10 ? (
         <p>Too many matches found, please specify more filter!</p>
